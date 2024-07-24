6661370249d040b4f99daa414ed79421222db5d1
PRIYANSH-2004 <priyanshsingh.pr@gmail.com>
2024-07-24 20:03:57 +0530

    videoCard implemented

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle} from '../utils/constants' //when we are missing data from api
+
+
+const VideoCard = ({video : {id : {videoId},snippet }}) => {
+    // console.log(videoId, snippet)
+    // console.log(snippet?.thumbnails?.high?.url)
+  return (
+    <Card>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia 
+        image={snippet?.thumbnails?.high?.url || demoVideoUrl}
+        alt={snippet?.title}
+        sx={{width:358 , height:180}}
+        />
+        </Link>
+    </Card>
+  )
+}
+
+export default VideoCard
